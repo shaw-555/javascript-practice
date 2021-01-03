2688e9d6fc14202aba798b7734d569542de7e389
shaw-555 <shaw18664590570@163.com>
2021-01-03 21:21:42 +0800

    fix: 改进版的promise同步下载

--- practice in Gaocheng4/promise/03.js
+++ practice in Gaocheng4/promise/03.js
@@ -1,28 +1,19 @@
 
-let promise1 = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve();
-            console.log('download a');
-        }, 5000);
+let promise1 = file('down a',5000).then(() => {
+        return file('down b',4000);
     }).then(() => {
-            return new Promise((resolve, reject) => {
-                    setTimeout(() => {
-                        resolve();
-                        console.log('download b');
-                    }, 1000);
-                })
-    }).then(() => {
-        return new Promise((resolve, reject) => {
+        return file('down c',3000);
+    }).then(()=>{
+        return file('down d',2000);
+    }).then(()=>{
+        return file('down e',1000);
+    })
+
+function file(name,time){
+        return new Promise((resolve,reject) => {
                 setTimeout(() => {
                     resolve();
-                    console.log('download c');
-                }, 2000);
-    }).then(()=>{
-        return new Promise((resolve,reject) => {
-            setTimeout(() => {
-                resolve();
-                console.log('download d');
-            }, 4000);
+                    console.log(name);
+                }, time);
         })
-    })
-})
+}
