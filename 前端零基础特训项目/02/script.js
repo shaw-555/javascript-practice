e1a21c354a8c8dd6a8c3a01b7f4523c244784293
shaw-555 <shaw18664590570@163.com>
2021-01-24 19:14:43 +0800

    fix:侧边栏项目

--- 前端零基础特训项目/02/script.js
+++ 前端零基础特训项目/02/script.js
@@ -9,15 +9,28 @@ let ticketPrice = Number(movieSelect.value);
 // 更新座位数及总票价
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  
+  const seatsIndex = [...selectedSeats].map( seat => {
+    return [...seats].indexOf(seat);
+  });
+
+  localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex));
+  
   const selectedSeatsCount = selectedSeats.length;
   console.log(selectedSeatsCount);
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// 保存电影的索引值和票价
+function setMovieData(movieIndex, moviePrice){
+  localStorage.setItem('selectedMovieIndex',movieIndex);
+  localStorage.setItem('selectedMoviePrcie'. moviePrice);
+}
 // 电影下拉框事件监听
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.seatsIndex,e.target.value);
   console.log(ticketPrice);
 })
 
