4a68501ef88c1b9e0df7310b4362a5a4071e0e8c
shaw-555 <shaw18664590570@163.com>
2021-03-05 21:27:23 +0800

    fix: 完成心情日历

--- 前端零基础特训项目/20/script.js
+++ 前端零基础特训项目/20/script.js
@@ -1,11 +1,11 @@
 const calendar = document.getElementById('calendar');
-const moods = document.querySelectorAll('.mood');
+const moods = document.querySelectorAll('.moods');
 const randomize = document.querySelector('#randomize');
 const clear = document.querySelector('#clear');
 
 
 // 创建日历所需的初始化变量
-const currentYear = 2020;
+const currentYear = 2021;
 const weekDays = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
 
 const months = [
@@ -18,11 +18,38 @@ const months = [
     'July',
     'August',
     'September',
+    'October',
     'November',
     'December'
 ];
 
 let monthsHTML = '';
+
+// 创建初始化心情日历相关变量
+const colors = ['#2d6b5f', '#72e3a6', '#dff4c7', '#edbf98', '#ea3d36'];
+const defaultColor = '#888';
+let activeColor = '';
+
+
+// 循环遍历心情图标
+moods.forEach( (mood) => {
+    mood.addEventListener('click',() => {
+        // 判断图标是否被选中
+        if (mood.classList.contains('selected')){
+            mood.classList.remove('selected');
+            activeColor = defaultColor;
+        } else {
+            moods.forEach( (mood) => {
+                mood.classList.remove('selected');
+            })
+            mood.classList.add('selected');
+        }
+       // 获取选中图标的颜色
+       activeColor = getComputedStyle(mood).getPropertyValue('color');
+       console.log(activeColor);
+    })
+})
+
 
 // 渲染日历的步骤
 // 1.获取这一年所有的日期
@@ -41,10 +68,29 @@ months.forEach((month,index) => {
 calendar.innerHTML = monthsHTML;
 
 // 3.每月日期渲染到对应的星期里面
-dates.forEach( date => {
+dates.forEach( (date) => {
     const month = date.getMonth();
-    
-})
+    const monthEl = document.querySelector(`.month_${month} .days_container`);
+
+    if(date.getDate() === 1 && date.getDay() !== 0) {
+        for( let i =0; i<date.getDay(); i++){
+            const emptySpot = createEmptySpot();
+            monthEl.appendChild(emptySpot);
+        }
+    }
+
+    const dateEl = createDateEl(date);
+    monthEl.appendChild(dateEl);
+});
+
+// 给日期添加颜色
+const circles = document.querySelectorAll('.circle');
+console.log(circles);
+circles.forEach((circle) => {
+    circle.addEventListener('click', () => {
+      circle.style.backgroundColor = activeColor;
+    });
+  });
 
 // ----------------函数------------------
 // getAllDays 获取所有日期
@@ -62,13 +108,24 @@ function getAllDays(year) {
     let lastDayInArray = firstDay;
 
     while(lastDayInArray.getTime() !== lastDay.getTime()){
-        console.log(1);
         days.push(addDays(lastDayInArray,1));
         lastDayInArray = days[days.length -1];
     }
 
     return days;
 }
+
+randomize.addEventListener('click' , () => {
+    circles.forEach((circle) => {
+        circle.style.backgroundColor = getRandomColor();
+    });
+})
+
+clear.addEventListener('click', () => {
+    circles.forEach( (circle) => {
+        circle.style.backgroundColor = defaultColor;
+    });
+})
 
 // 日期自增函数
 function addDays(date, days){
@@ -77,3 +134,22 @@ function addDays(date, days){
     return result;
 }
 
+// createEmptySpot 创建空白站位日期
+function createEmptySpot(){
+    const emptyEl = document.createElement('div');
+    emptyEl.classList.add("days");
+    emptyEl.innerText = ' ';
+    return emptyEl;
+}
+
+function createDateEl(date){
+    const day= date.getDate();
+    const dateEl= document.createElement('div');
+    dateEl.classList.add('days');
+    dateEl.innerHTML = `<span class="circle">${day}</span>`;
+    return dateEl;
+}
+
+function getRandomColor(){
+    return colors[Math.floor(Math.random()*5)];
+}
