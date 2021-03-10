da1dfd34e8299efc0e4d8f3c2b33dcc11697efc7
shaw-555 <shaw18664590570@163.com>
2021-03-10 14:50:27 +0800

    fix: 完成,需要加强对拖拽的理解

--- 前端零基础特训项目/13/script.js
+++ 前端零基础特训项目/13/script.js
@@ -8,81 +8,84 @@ const closeBtn = document.getElementById('close');
 
 // 创建Data数据
 const data = [
-    {
-      image: "./img/spring01.jpg",
-      text: "春暖花开"
-    },
-    {
-      image: "./img/summer01.jpg",
-      text: "夏阳酷暑"
-    },
-    {
-      image: "./img/autumn01.jpg",
-      text: "雁过留声"
-    },
-    {
-      image: "./img/winter01.jpg",
-      text: "白雪皑皑"
-    },
-    {
-      image: "./img/spring02.jpg",
-      text: "草长莺飞"
-    },
-    {
-      image: "./img/summer02.jpg",
-      text: "骄阳似火"
-    },
-    {
-      image: "./img/autumn02.jpg",
-      text: "一叶知秋 "
-    },
-    {
-      image: "./img/winter02.jpg",
-      text: "瑞雪纷飞"
-    },
-    {
-      image: "./img/spring03.jpg",
-      text: "鸟语花香"
-    },
-    {
-      image: "./img/summer03.jpg",
-      text: "艳阳高照"
-    },
-    {
-      image: "./img/autumn03.jpg",
-      text: "秋风瑟瑟"
-    },
-    {
-      image: "./img/winter03.jpg",
-      text: "雪中送炭"
-    }
-  ];
+  {
+    image: "./img/spring01.jpg",
+    text: "春暖花开"
+  },
+  {
+    image: "./img/summer01.jpg",
+    text: "夏阳酷暑"
+  },
+  {
+    image: "./img/autumn01.jpg",
+    text: "雁过留声"
+  },
+  {
+    image: "./img/winter01.jpg",
+    text: "白雪皑皑"
+  },
+  {
+    image: "./img/spring02.jpg",
+    text: "草长莺飞"
+  },
+  {
+    image: "./img/summer02.jpg",
+    text: "骄阳似火"
+  },
+  {
+    image: "./img/autumn02.jpg",
+    text: "一叶知秋 "
+  },
+  {
+    image: "./img/winter02.jpg",
+    text: "瑞雪纷飞"
+  },
+  {
+    image: "./img/spring03.jpg",
+    text: "鸟语花香"
+  },
+  {
+    image: "./img/summer03.jpg",
+    text: "艳阳高照"
+  },
+  {
+    image: "./img/autumn03.jpg",
+    text: "秋风瑟瑟"
+  },
+  {
+    image: "./img/winter03.jpg",
+    text: "雪中送炭"
+  }
+];
 
-  data.forEach(createBox);
+data.forEach(createBox);
 
 // 创建盒子
-function createBox(item){
-    const box = document.createElement('div');
+function createBox(item) {
+  const box = document.createElement('div');
 
-    const {image, text} = item;
+  const {
+    image,
+    text
+  } = item;
 
-    box.classList.add('box');
+  box.classList.add('box');
 
-    box.innerHTML = `
+  box.innerHTML = `
         <img src="${image}" alt="${text}" />
         <p class="info>${text}</p>
     `
 
-    // 点击按钮进行阅读
-    box.addEventListener('click', () => {
-      setTextMessage(text);
-      speakText();
+  // 点击按钮进行阅读
+  box.addEventListener('click', () => {
+    setTextMessage(text);
+    speakText();
 
-      // 添加acitive
-      box.classList.add('active');
-      setTimeout(()=>box.classList.remove('active'),800);
-    })
-    main.appendChild(box);
+    // 添加acitive
+    box.classList.add('active');
+    setTimeout(() => box.classList.remove('active'), 800);
+  })
+  main.appendChild(box);
 }
 
 // 初始化speechsynth
@@ -90,35 +93,36 @@ const message = new SpeechSynthesisUtterance();
 
 // 创建空数组存储语音
 let voices = [];
-function getVoices(){
-    voices = speechSynthesis.getVoices();
 
-    voices.forEach(voice => {
-      const option = document.createElement('option');
-      option.value = voice.name;
-      option.innerText = `${voice.name} ${voice.lang}`;
-      console.log(option);
-      voicesSelect.appendChild(option);
-    })
+function getVoices() {
+  voices = speechSynthesis.getVoices();
+
+  voices.forEach(voice => {
+    const option = document.createElement('option');
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+    console.log(option);
+    voicesSelect.appendChild(option);
+  })
 }
 
 // 获得文字
-function setTextMessage(text){
+function setTextMessage(text) {
   message.text = text;
 }
- 
+
 // 阅读文字
-function speakText(){
+function speakText() {
   speechSynthesis.speak(message);
 }
 
 // 切换语音
-function setVoice(e){
+function setVoice(e) {
   message.voice = voices.find(voice => voice.name === e.target.value)
 }
 
 // 切换语音事件监听
-speechSynthesis.addEventListener('voiceschanged',getVoices);
+speechSynthesis.addEventListener('voiceschanged', getVoices);
 
 // 切换文字框事件监听
 toggleBtn.addEventListener("click", () => {
@@ -126,12 +130,12 @@ toggleBtn.addEventListener("click", () => {
 });
 
 // 关闭按钮事件监听
-closeBtn.addEventListener('click',() => {
-    document.getElementById("text-box").classList.remove("show");
+closeBtn.addEventListener('click', () => {
+  document.getElementById("text-box").classList.remove("show");
 })
 
 // select下拉框的事件监听
-voicesSelect.addEventListener('change',setVoice);
+voicesSelect.addEventListener('change', setVoice);
 
 // 阅读文字按钮的事件监听
 readBtn.addEventListener('click', () => {
--- 前端零基础特训项目/18/script.js
+++ 前端零基础特训项目/18/script.js
@@ -26,18 +26,94 @@ createList();
 // 创建createList函数，将li插入到DOM节点中
 
 function createList() {
-    [...richestPeople].forEach({person,index} => {
+    [...richestPeople].sort()
+        .forEach((person, index) => {
         const listItem = document.createElement('li');
-        listItem.setAttribute('data-index',index);
-
+        listItem.setAttribute('data-index', index);
         listItem.innerHTML = `
-        <span class="number">${index+1}</span>
-        <div draggable="true" class="draggable"></div> 
-        <p class="person-name>${person}</p>
-        <i class="fas fa-grip-line"></i>
-        `
+            <span class="number">${index + 1}</span>
+                <div draggable="true" class="draggable">
+                <p class="person-name">${person}</p>
+                <i class="fas fa fa-grip-line"></i>
+            </div> 
+            `;
 
         listItems.push(listItem);
         draggable_list.appendChild(listItem);
-    });
+        });
+        // 事件监听函数
+    addEventListeners(); 
 }
+
+function dragStart() {
+    // console.log("event:", "dragStart");
+    dragStartIndex = this.closest('li').getAttribute('data-index');
+    console.log(dragStartIndex);
+}
+
+function dragEnter() {
+    //console.log("event:", "dragEnter");
+    this.classList.add('over');
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+    //console.log("event:", "dragLeave");
+    this.classList.remove('over');
+}
+
+function dragDrop() {
+    // console.log("event:", "dragDrop");
+    const dragEndIndex = this.getAttribute("data-index");
+    console.log(dragEndIndex);
+    swapItems(dragStartIndex, dragEndIndex);
+    this.classList.remove("over");
+}
+
+function swapItems(fromIndex, toIndex) {
+    const itemOne = listItems[fromIndex].querySelector(".draggable");
+    const itemTwo = listItems[toIndex].querySelector(".draggable");
+
+    //   console.log(itemOne, itemTwo);
+    listItems[fromIndex].appendChild(itemTwo);
+    listItems[toIndex].appendChild(itemOne);
+}
+
+function checkOrder() {
+    listItems.forEach((listItem, index) => {
+        const personName = listItem.querySelector('.draggable').innerText.trim();
+        
+        if (personName == richestPeople[index]){
+            console.log(1);
+            console.log(listItem);
+            listItem.classList.add('right');
+        } else {
+            listItem.classList.remove('right');
+            listItem.classList.add('wrong');
+        }
+    });
+
+}
+
+// 创建事件监听函数
+function addEventListeners() {
+    console.log(1);
+    const draggables = document.querySelectorAll('.draggable');
+    const dragListItems = document.querySelectorAll('.draggable-list li');
+    console.log(dragListItems, draggables);
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', dragStart);
+    })
+
+    dragListItems.forEach(item => {
+        item.addEventListener('dragenter', dragEnter);
+        item.addEventListener('dragover', dragOver);
+        item.addEventListener('dragleave', dragLeave);
+        item.addEventListener('drop', dragDrop);
+    })
+}
+
+check.addEventListener('click', checkOrder);
