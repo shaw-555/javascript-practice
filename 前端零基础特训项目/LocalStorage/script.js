55e786a9db09f15947661c4402e9f8e1265cfabd
shaw-555 <shaw18664590570@163.com>
2021-03-29 20:25:17 +0800

    响应式grid

---
+++ 前端零基础特训项目/LocalStorage/script.js
@@ -0,0 +1,11 @@
+localStorage.setItem("name", "shaw");
+localStorage.setItem("age", "18");
+
+//sessionStorage 存储
+sessionStorage.setItem("name", "shaw")
+
+localStorage.removeItem("age");
+
+
+localStorage.clear();
+document.write(localStorage.getItem("name"));
