21559a48f69d0531bfa5547d5d23364d673a2aa3
shaw-555 <shaw18664590570@163.com>
2021-04-15 20:30:13 +0800

    ES6 基础

---
+++ 前端零基础特训项目/ES6/let.js
@@ -0,0 +1,9 @@
+for (var i = 0; i < 10; i++){
+    i = 5;
+    console.log(i)
+}
+
+for (let j = 0; j < 10; j++){
+    j = 5;
+    console.log(j)
+}
---
+++ 前端零基础特训项目/ES6/template.js
@@ -0,0 +1,7 @@
+let li = `<li>${UpperCase('hehe')}</li>`;
+
+function UpperCase(word) {
+    return word.toLocaleUpperCase()
+}
+
+console.log(li);
